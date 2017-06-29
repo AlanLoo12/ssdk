ad0908bce3f3f64725e932edf69b4d9c5993431a
Danil P <slemonide@gmail.com>
2017-06-28 22:20:02 -0700

    Fix the bug that allowed to type more than one period in a float number.

--- src/gui_elements/FloatField.java
+++ src/gui_elements/FloatField.java
@@ -12,7 +12,7 @@ public class FloatField extends TextField {
         this.addEventFilter(KeyEvent.KEY_TYPED, t -> {
             char ar[] = t.getCharacter().toCharArray();
             char ch = ar[t.getCharacter().toCharArray().length - 1];
-            if (!((ch >= '0' && ch <= '9') || ch == '.')) {
+            if (!((ch >= '0' && ch <= '9') || (ch == '.' & !getText().contains(".")))) {
                 t.consume();
             }
         });
